1631319a00cd59476f013d89762393d2fe6d7a8a
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-04-06 13:41:10 +0200

    passing test for depositing money and balance increasing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,15 +3,15 @@
 require 'account'
 
 describe 'account' do
-  describe '#balance' do
-    it 'should initially have a balance of 0' do
+  describe 'balance' do
+    it 'should initially be 0' do
       account = Account.new
       expect(account.balance).to eq 0
     end
     it 'should increase when money is deposited' do
-        account = Account.new
-        account.deposit(20)
-        expect(account.balance).to eq 20
+      account = Account.new
+      account.deposit(20)
+      expect(account.balance).to eq 20
     end
   end
 end
