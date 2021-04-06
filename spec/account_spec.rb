bd66790f80cd3345d7d48239b396c173cda0b69d
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-04-06 13:53:09 +0200

    failing test for balance to decrease when money withdrawn

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,9 @@
 require 'account'
 
 describe 'account' do
+
+    
+
   describe 'balance' do
     it 'should initially be 0' do
       account = Account.new
@@ -12,6 +15,12 @@ describe 'account' do
       account = Account.new
       account.deposit(20)
       expect(account.balance).to eq 20
+    end
+    it 'should decrease when money is withdrawn' do
+      account = Account.new
+      account.deposit(20)
+      account.withdraw(5)
+      expect(account.balance).to eq 15
     end
   end
 end
