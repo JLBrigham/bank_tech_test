1a08f84469e770802891ee0b197359780eccad8e
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-04-06 13:55:21 +0200

    passing test for balance decreasing when money withdrawn and refactor tests to keep DRY

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,20 +4,17 @@ require 'account'
 
 describe 'account' do
 
-    
+    let (:account) {Account.new}
 
   describe 'balance' do
     it 'should initially be 0' do
-      account = Account.new
       expect(account.balance).to eq 0
     end
     it 'should increase when money is deposited' do
-      account = Account.new
       account.deposit(20)
       expect(account.balance).to eq 20
     end
     it 'should decrease when money is withdrawn' do
-      account = Account.new
       account.deposit(20)
       account.withdraw(5)
       expect(account.balance).to eq 15
